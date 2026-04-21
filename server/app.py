8cf7fcc2b2f49604a3956d6ce123bba2304b9d3e
Sanaya James <sanaya.j.james@gmail.com>
2026-04-20 20:38:12 -0700

    completed last step and magnitude test passed

--- server/app.py
+++ server/app.py
@@ -41,6 +41,19 @@ def earthquake_by_id(id):
 
     return make_response(body, status)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_mag(magnitude):
+    quakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+        quake_dict = {'id': quake.id,
+                      'location': quake.location,
+                      'magnitude':  quake.magnitude,
+                      'year': quake.year
+                      }
+        quakes.append(quake_dict)
+    body = {'count': len(quakes),
+            'quakes': quakes}
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
